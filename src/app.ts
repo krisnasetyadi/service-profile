245774abbb0f94f5bc6d889964ad35293b4987a4
Krisna Setyaadi <krisnadwisetyadi@gmail.com>
2025-04-22 11:08:43 +0700

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const upload = multer();
 app.listen(process.env.PORT, () => {
   console.log(`server is running on port ${process.env.PORT}`);
 });
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(upload.any());
